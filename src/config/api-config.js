97fbb72b9fbfc0eebad9c491becf2a4dd6bb8af1
duonga6 <shenmurai1st@gmail.com>
2024-03-04 00:20:51 +0700

    change api url

--- src/config/api-config.js
+++ src/config/api-config.js
@@ -1,6 +1,6 @@
 function getApiUrl() {
   if (process.env.NODE_ENV == "production") {
-    return "https://facebookapi.duonga6.top/api";
+    return "https://fbapi.duonga6.top/api";
   }
 
   return "http://localhost:9999/api";
